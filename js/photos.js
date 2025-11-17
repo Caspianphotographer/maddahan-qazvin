ad45802a904cee702a8a75f70f8e9f11f614a951
Caspianphotographer <amirphotomahdi@gmail.com>
2025-11-17 12:25:19 +0330

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -1,1 +1,15 @@
+async function loadPhotos() {
+    const res = await fetch("data/photos.json");
+    const photos = await res.json();
 
+    let box = document.getElementById("photo-gallery");
+    photos.forEach(p => {
+        box.innerHTML += `
+            <div>
+                <img src="${p.url}" alt="${p.title}">
+                <p>${p.title}</p>
+            </div>
+        `;
+    });
+}
+loadPhotos();
