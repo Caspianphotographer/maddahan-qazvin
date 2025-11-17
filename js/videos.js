3394b895230783b26bcd0968023c273e00df6029
Caspianphotographer <amirphotomahdi@gmail.com>
2025-11-17 12:25:32 +0330

    Update videos.js

--- js/videos.js
+++ js/videos.js
@@ -1,1 +1,17 @@
+async function loadVideos() {
+    const res = await fetch("data/videos.json");
+    const videos = await res.json();
 
+    let box = document.getElementById("video-gallery");
+
+    videos.forEach(v => {
+        box.innerHTML += `
+            <div class="video-box">
+                <h3>${v.title}</h3>
+                <iframe src="${v.url}" allowfullscreen></iframe>
+            </div>
+        `;
+    });
+}
+
+loadVideos();
